f80b11c332b34735d66556760f21da458f494973
Bhavith Shetty <bhavithshetty2203@gmail.com>
2025-05-21 20:42:44 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    DEEPSEEK_API_KEY: "sk-48ed5f18ee5e47b5bd6c2f9227e7b212",
+    DEEPSEEK_API_KEY: "",
   },
   eslint: {
     ignoreDuringBuilds: true,
